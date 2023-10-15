3b78bfd433721ea6fae3631932503361d633e1e0
Tony C. Batista <tony_cb@ymail.com>
2023-10-15 16:07:27 -0300

    Implementing json parser to parse api input

---
+++ pkg/infra/flightparser/jsonparser.go
@@ -0,0 +1,46 @@
+package flightparser
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/pkg/errors"
+
+	"github.com/tonytcb/flight-path-tracker/pkg/domain"
+)
+
+type JSONParser struct {
+}
+
+func (p *JSONParser) Parse(ctx context.Context, raw []byte) (domain.Flights, error) {
+	type flight struct {
+		Source      string `json:"source"`
+		Destination string `json:"destination"`
+	}
+
+	var payload []*flight
+	if err := json.Unmarshal(raw, &payload); err != nil {
+		return nil, errors.Wrap(err, "error to json decode payload")
+	}
+
+	var output = make([]*domain.Flight, 0)
+	for k, v := range payload {
+		select {
+		case <-ctx.Done():
+			return nil, errors.Wrap(ctx.Err(), "context done while parsing payload")
+		default:
+		}
+
+		if v.Source == "" {
+			return nil, errors.Errorf("source value can not be empty on flight number %d", k)
+		}
+
+		if v.Destination == "" {
+			return nil, errors.Errorf("destination value can not be empty on flight number %d", k)
+		}
+
+		output = append(output, domain.NewFlight(domain.Airport(v.Source), domain.Airport(v.Destination)))
+	}
+
+	return output, nil
+}
---
+++ pkg/infra/flightparser/jsonparser_test.go
@@ -0,0 +1,90 @@
+package flightparser
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/tonytcb/flight-path-tracker/pkg/domain"
+)
+
+func TestJSONParser_Parse(t *testing.T) {
+	t.Parallel()
+
+	canceledCtx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	type args struct {
+		ctx context.Context
+		raw []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    domain.Flights
+		wantErr bool
+	}{
+		{
+			name: "should parse a json containing two flights successfully ",
+			args: args{
+				ctx: context.Background(),
+				raw: []byte(`[{"source":"IND","destination":"EWR"},{"source":"SFO","destination":"ATL"}]`),
+			},
+			want: []*domain.Flight{
+				{
+					Source:      "IND",
+					Destination: "EWR",
+				},
+				{
+					Source:      "SFO",
+					Destination: "ATL",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "should parse an empty json successfully",
+			args: args{
+				ctx: context.Background(),
+				raw: []byte(`[]`),
+			},
+			want:    []*domain.Flight{},
+			wantErr: false,
+		},
+		{
+			name: "should error on an invalid json",
+			args: args{
+				ctx: context.Background(),
+				raw: []byte(`invalid json`),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "should error on context done",
+			args: args{
+				ctx: canceledCtx,
+				raw: []byte(`[{"source":"IND","destination":"EWR"},{"source":"SFO","destination":"ATL"}]`),
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			p := &JSONParser{}
+			got, err := p.Parse(tt.args.ctx, tt.args.raw)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Parse() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Parse() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
